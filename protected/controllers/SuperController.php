980a4cd6d00151d6685a741af31d36bc584e459f
huynt <huynt57@gmail.com>
2015-07-24 09:55:35 +0700

    add API

--- protected/controllers/SuperController.php
+++ protected/controllers/SuperController.php
@@ -29,7 +29,7 @@ class SuperController extends Controller {
                     $sick = PatientSick::model()->findAllByAttributes(array('patient_id' => $patient["patient_id"]));
                     // var_dump($sick);
                     $inject = PatientInjection::model()->findAllByAttributes(array('patient_id' => $patient["patient_id"]));
-                   // var_dump($inject);
+                    // var_dump($inject);
                     array_push($inject_data, $inject);
                     array_push($sick_data, $sick);
                     // die();
@@ -42,6 +42,85 @@ class SuperController extends Controller {
             }
             echo CJSON::encode($this->retVal);
             Yii::app()->end();
+        }
+    }
+
+    public function actionUpdateAppDbVer() {
+        $this->retVal = new stdClass();
+        $request = Yii::app()->request;
+        if ($request->isPostRequest && isset($_POST)) {
+            try {
+                
+            } catch (exception $e) {
+                $this->retVal->message = $e->getMessage();
+            }
+            echo CJSON::encode($this->retVal);
+            Yii::app()->end();
+        }
+    }
+
+    public function actionCreatePatientAndSick() {
+        $this->retVal = new stdClass();
+        $request = Yii::app()->request;
+        if ($request->isPostRequest && isset($_POST)) {
+            try {
+                $name = StringHelper::filterString($request->getPost('name'));
+                $dob = StringHelper::filterString($request->getPost('dob'));
+                $gender = StringHelper::filterString($request->getPost('gender'));
+                $user_id = StringHelper::filterString($request->getPost('user_id'));
+                $relation = StringHelper::filterString($request->getPost('relationshipWithUser'));
+                $blood = StringHelper::filterString($request->getPost('bloodType'));
+                $sicks = StringHelper::filterString($request->getPost('sicks'));
+
+                $patient_model = new Patient;
+                $patient_model->name = $name;
+                $patient_model->dob = $dob;
+                $patient_model->gender = $gender;
+                $patient_model->last_updated = time();
+                $patient_model->bloodType = $blood;
+                $patient_model->relationshipWithUser = $relation;
+                $patient_model->save(FALSE);
+
+                $user_patient = new UserPatient;
+                $user_patient->user_id = $user_id;
+                $user_patient->patient_id = $patient_model->patient_id;
+                $user_patient->save(FALSE);
+
+                $sick_arr = json_decode($sicks);
+                foreach ($sick_arr as $sick) {
+                    $model = new PatientSick();
+                    $model->patient_id = $user_patient->patient_id;
+                    $model->sick_id = $sick;
+                    $model->save(FALSE);
+                    $this->createScheduleSick($sick, $patient_id);
+                }
+
+                $this->retVal->message = "Success";
+                $this->retVal->patient_id = $patient_model->patient_id;
+            } catch (exception $e) {
+                $this->retVal->message = $e->getMessage();
+            }
+            echo CJSON::encode($this->retVal);
+            Yii::app()->end();
+        }
+    }
+
+    public function createScheduleSick($sick_id, $patient_id) {
+        $sick_infos = InjectionScheduler::model()->findAllByAttributes(array('sick_id' => $sick_id));
+        $patient_info = Patient::model()->findByAttributes(array('patient_id' => $patient_id));
+
+        foreach ($sick_infos as $sick_info) {
+            $model = new PatientInjection;
+            $model->sick_id = $sick_id;
+            $model->patient_id = $patient_id;
+            $model->number = $sick_info->number;
+            $model->done = 0;
+            $model->month = $sick_info->month;
+            $date = new DateTime($patient_info->dob);
+            $date->modify('+' . $sick_info->month . ' month');
+            $model->inject_day = $date->format('d-m-Y');
+            $model->last_updated = time();
+            $model->save(FALSE);
         }
     }
 
